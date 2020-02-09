cd783a5bf3f349149a4af0667238e12be7943d68
diwakar-kumar-au3 <dbdiwakarbhagat@gmail.com>
2020-02-09 19:39:31 +0530

    navbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "./home";
+import Nav from "./nav";
 import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <div>
       <div className="App" style={{ background: "#E6E8E6" }}>
         <Router>
+          <Nav />
           <Home />
         </Router>
       </div>
